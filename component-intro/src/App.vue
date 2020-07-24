4a4bad2bf00a0d7d34f9de8f1212bfe1d1a1f836
higorasilverio <higorasilverio@outlook.com>
2020-07-24 17:33:21 -0300

    Introduction to the first CLI project

---
+++ component-intro/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h1>Counters</h1>
+    {{ counter }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      counter: 0
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
